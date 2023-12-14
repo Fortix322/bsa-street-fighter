e1ea1691a5640d280adf16f989f7c1401a582b0e
Fortix322 <54719846+Fortix322@users.noreply.github.com>
2023-12-14 21:11:10 +0200

    + implemented createFighterPreview; stylized fighter preview

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,6 +7,36 @@ export function createFighterPreview(fighter, position) {
         className: `fighter-preview___root ${positionClassName}`
     });
 
+    if (fighter) {
+        const flippedImage = position === 'right' ? 'fighter-preview__fighter-img-flipped' : '';
+        const alignedText = position === 'right' ? 'fighter-preview__fighter-info-right' : '';
+
+        const imageElement = createElement({
+            tagName: 'img',
+            className: `fighter-preview__fighter-img ${flippedImage}`
+        });
+
+        const infoElement = createElement({
+            tagName: 'h1',
+            className: `fighter-preview__fighter-info ${alignedText}`
+        });
+
+        imageElement.setAttribute('src', fighter.source);
+
+        let info = { ...fighter };
+
+        delete info._id;
+        delete info.source;
+
+        info = JSON.stringify(info);
+        info = info.slice(1, -1);
+        info = info.replace(/"/g, '');
+        info = info.replace(/,/g, '\n');
+        infoElement.innerHTML = info;
+
+        fighterElement.appendChild(imageElement);
+        fighterElement.appendChild(infoElement);
+    }
     // todo: show fighter info (image, name, health, etc.)
 
     return fighterElement;
